421826a4ff90a4edbdc563bd80e734633679cbe0
Alexandru Albu <alexandru.albu@allcloud.io>
2023-11-13 15:22:46 +0000

    add CV to static files

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='src/static')
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def serve_cv():
+    return send_from_directory(app.static_folder, 'Alexandru_ALBU_CV.pdf')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
