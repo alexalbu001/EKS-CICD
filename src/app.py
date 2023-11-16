c8df84a490e831a99c9aa42e84954aff5d39e055
Alexandru Albu <alexandru.albu@allcloud.io>
2023-11-16 14:12:59 +0000

    add grafana and prometheus deployments, clusterrole and rolebinding for prome and change app to CV

--- src/app.py
+++ src/app.py
@@ -1,11 +1,10 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
-#route of static
-app = Flask(__name__, static_folder='src/static')
+app = Flask(__name__)
 
 @app.route('/')
-def serve_cv():
-    return send_from_directory(app.static_folder, 'Alexandru_ALBU_CV.pdf')
+def cv():
+    return render_template('srt-resume.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
